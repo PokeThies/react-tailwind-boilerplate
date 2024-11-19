8290f15255f1eae18adadad294a80c3289363234
Poke <134518374+PokeThies@users.noreply.github.com>
2024-11-19 22:00:25 +0100

    Update Layout.jsx

--- tailwind-react-boilerplate/my-project/src/layout/Layout.jsx
+++ tailwind-react-boilerplate/my-project/src/layout/Layout.jsx
@@ -5,7 +5,7 @@ import {Outlet} from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div>
+    <>
         <Header/>
 
         <main>
@@ -13,7 +13,7 @@ const Layout = () => {
         </main>
 
         <Footer/>
-    </div>
+    </>
   )
 }
 
