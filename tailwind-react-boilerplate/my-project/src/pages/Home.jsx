edfc73f867085727bbb7a11cfc67cb42df9d90f8
Poke <134518374+PokeThies@users.noreply.github.com>
2024-11-19 21:59:00 +0100

    Update Home.jsx

--- tailwind-react-boilerplate/my-project/src/pages/Home.jsx
+++ tailwind-react-boilerplate/my-project/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Loader from '../components/Loader'
 
 const Home = () => {
   return (
-    <div className='text-3xl text-center pt-96 text-white  '>
+    <div className='text-3xl text-center pt-96 text-white'>
 
      Iver's React Tailwind Boilerplate
       
