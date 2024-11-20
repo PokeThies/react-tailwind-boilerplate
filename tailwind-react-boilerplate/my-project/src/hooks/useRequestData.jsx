344902e2ebb1be837e69eee5368158552c85dff5
Poke <134518374+PokeThies@users.noreply.github.com>
2024-11-20 08:50:57 +0100

    Update useRequestData.jsx

--- tailwind-react-boilerplate/my-project/src/hooks/useRequestData.jsx
+++ tailwind-react-boilerplate/my-project/src/hooks/useRequestData.jsx
@@ -1,71 +1,75 @@
-import { useState } from "react";
-import axios from "axios";
+import { useState } from "react"
+import axios from "axios"
 
 const useRequestData = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
 
-  /**
-   * Lav en request til en api
-   * @param {string} url link til api'en
-   * @param {string} method  hvilken måde du ville bruge api'en på (GET, POST, DELETE)'
-   * @param {JSON} bodydata Data der skal sendes til api'en
-   * @param {JSOn} headers
-   * @param {JSON} params Special data til api'en så som api key
-   */
-  const makeRequest = async (url, method = "GET", bodydata = null) =>
-    /* Funktionen async giver din browser mulighed for at køre funkcionen i bagrudnen mens man bruger/kigger på hjemmesiden */
+    const [ isLoading, setIsLoading] = useState(false);
+    const [ data, setData] = useState(null);
+    const [ error, setError] = useState(null);
+
+    /**
+     * lav en request til en api
+     * @param {string} url link til api'en
+     * @param {string} method hvilken måde du vil bruge api'en på (GET, POST, DELETE)
+     * @param {JSON} bodydata data der skal sendes til api'en
+     * @param {JSON} headers 
+     * @param {JSON} params spcial data til api'en så som api key 
+     */
+    const makeRequest = async (url, method = "GET", bodydata = null, headers = null, params = null) =>
+    //funktionen er async fordi det giver browseren mulighed for at køre functionen i bagrunden mens brugeren kigger på/bruger hjemmesiden
     {
-      setIsLoading(true);
+        setIsLoading( true );
 
-      try {
-        let response; /* en let som kommer til at indeholde reponse fra api'en */
+        try
+        {
+            let response;//en let som kommer til at indeholde response fra api'en
 
-        switch (
-          method // gør det samme som en masse elif statements men er mere performance velligt når man kommer over 3 statements
-        ) {
-          case "GET":
-            response = await axios.get(url, { headers, params }); // await betyder at linjen skal vente på den asynkrone funktion bliver færdig
+            switch(method){// gør det samme som en masse elif statements men er mere performance velligt når man kommer over 3 statements
+                case "GET":
+                    response = await axios.get(url, { headers, params });//await betyder at linjen skal vente på den asynkrone funktion bliver færdig
+                    break;
 
-            break;
-          case "POST":
-            response = await axios.post(url, bodydata, { headers, params });
+                case "POST":
+                    response = await axios.post(url, bodydata, { headers, params });
+                    break;
 
-            break;
-          case "DELETE":
-            response = await axios.delete(url, { headers, params });
+                case "DELETE":
+                    response = await axios.delete(url, { headers, params });
+                    break;
 
-            break;
-          case "PUT":
-            response = await axios.put(url, bodydata, { headers, params });
+                case "PUT":
+                    response = await axios.put(url, bodydata, { headers, params });
+                    break;
 
-            break;
-          case "patch":
-            response = await axios.patch(url, bodydata, { headers, params });
+                case "PATCH":
+                    response = await axios.patch(url, bodydata, { headers, params });
+                    break;
 
-            break;
-          default:
-            throw new Error(
-              "Invalid method. GET, POST, PUT, PATCH or DELETE was expected"
-            );
+                default:
+                    throw new Error( "Invalid method. GET, POST, PUT, PATCH or DELETE was expected.");
+            }
+
+            if( response && response.data !== undefined){
+                setData( response.data );
+                setError( null );
+            }
+            else{
+                throw new Error("Empty rensponse or no data returned");
+            }
         }
-        if (response && response.data !== undefined) {
-          setData(response.data);
-          setError(null);
-        } else {
-          throw new Error("Empty response or no data returned");
+        catch (error)//hvis der opstår en fejl kørrer vi denne kode for at vise det til brugeren og os selv
+        {
+            console.log( "ERROR", error );
+            setError( "an error has ocurred: " + error.message);
+            setData(null);
         }
-      } catch (error) {
-        console.log("Fejl", error);
-        setError("An error has ocured:" + error.message);
-        setData(null);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+        finally//bliver kørt i slutningen uanset om vi har fået en fejl eller ej
+        {
+            setIsLoading(false);
+        }
+    }
 
-  return {makeRequest, isLoading, data, error}; // sender den data tilbage som skal bruges yden for dette script som sender det tilbage som et JSON ibjet (Den data inden indefor tuborg klammerne)
+    return { makeRequest, isLoading, data, error };//sender den data tilbage som skal bruges uden for dette script som et JSON objekt(den data inden i tuborg klapperne)
 }
 
-export default useRequestData;
+export default useRequestData
