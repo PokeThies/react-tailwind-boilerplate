d3143109792cae615bb8fee9e73344f6651a11d8
Poke <134518374+PokeThies@users.noreply.github.com>
2025-03-27 22:10:41 +0100

    Add files via upload

---
+++ react-tailwind-boilerplate-main/tailwind-react-boilerplate/my-project/src/layout/Admin/AdminFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminFooter = () => {
+  return (
+    <section>
+      
+    </section>
+  )
+}
+
+export default AdminFooter
---
+++ react-tailwind-boilerplate-main/tailwind-react-boilerplate/my-project/src/layout/Admin/AdminHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminNavbar from "../Admin/AdminNavbar"
+
+const AdminHeader = () => {
+  return (
+    <section>
+      <AdminNavbar/>
+    </section>
+  )
+}
+
+export default AdminHeader
